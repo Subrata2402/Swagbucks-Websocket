9bd325fb658cd7335d64fc8862469c21fbae9ec2
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-10-16 19:23:52 +0530

    Create swagbucks.py

---
+++ swagbucks.py
@@ -0,0 +1,136 @@
+import discord
+from Websocket.swagbucks_ws import SbWebSocket, SwagbucksLive
+from discord.ext import commands
+import db
+import config
+# import server
+
+class SwagbucksTrivia(commands.Cog, SwagbucksLive):
+	
+	def __init__(self, client: commands.Bot):
+		super().__init__(client)
+		self.client = client
+		self.usernames = ["bernfried3250ber", "baldric3250schn4", "bernfried32", "sakhman32", "sakhman3250s1", "sakhman3250s2"]
+
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print(self.client.user.name)
+		game = discord.Game("Swagbucks Live")
+		await self.client.change_presence(status=discord.Status.dnd, activity=game)
+		for username in self.usernames:
+			ws = SbWebSocket(self.client, username)
+			self.client.loop.create_task(ws.connect_websocket())
+
+	# @commands.command()
+	# @commands.is_owner()
+	# async def sbstart(self, ctx: commands.Context, username: str = None):
+	# 	"""
+	# 	Check and open a websocket by username.
+	# 	"""
+	# 	if not username:
+	# 		return await ctx.send("username is required!")
+	# 	ws = SbWebSocket(self.client, username)
+	# 	await ws.get_ws()
+	# 	if ws.ws:
+	# 		if ws.ws.open:
+	# 			return await ws.send_hook("Websocket Already Opened!")
+	# 	await ws.send_hook("Websocket Connecting...")
+	# 	await ws.connect_websocket()
+		
+		
+	# @commands.command()
+	# @commands.is_owner()
+	# async def sbclose(self, ctx: commands.Context, username: str = None):
+	# 	"""
+	# 	Close a websocket by username.
+	# 	"""
+	# 	ws = SbWebSocket(self.client, username.lower())
+	# 	await ws.close_ws()
+	
+	# @commands.command()
+	# @commands.is_owner()
+	# async def sblogin(self, ctx, email_id: str = None, password: str = None):
+	# 	"""
+	# 	Login a Swagbucks account and stored some required details in the database.
+	# 	"""
+	# 	if not email_id or not password:
+	# 		return await ctx.send("Username or Password is required to login to Swagbucks.")
+	# 	await self.login(email_id, password)
+		
+	@commands.command()
+	@commands.is_owner()
+	async def sbupdate(self, ctx: commands.Context, username: str = None):
+		"""
+		If account is expire then this command will delete the stored account details
+		and login again to update account.
+		"""
+		if not username:
+			return await ctx.send("Required username to update of Swagbucks account.")
+		await self.update_account(username)
+		
+	@commands.command()
+	@commands.is_owner()
+	async def sbdetails(self, ctx: commands.Context, username: str = None):
+		"""
+		Get stats details of a Swagbucks account.
+		"""
+		if not username:
+			return await ctx.send("Required username to get details of Swagbucks account.")
+		await self.account_details(username.lower())
+		
+	
+	@commands.command()
+	@commands.is_owner()
+	async def sbaccounts(self, ctx: commands.Context):
+		"""
+		Get all accounts username, stored in the database.
+		"""
+		accounts = list(db.sb_details.find())
+		description = ""
+		for index, data in enumerate(accounts):
+			description += "{}{} - {}\n".format(0 if index+1 < 10 else "", index+1, data["username"])
+		if not accounts:
+			return await ctx.send("No accounts found.")
+		await ctx.send("```\n{}\n```".format(description))
+		
+	@commands.command()
+	@commands.is_owner()
+	async def sbbal(self, ctx: commands.Context):
+		"""
+		Get all accounts Swagbucks details, stored in the database.
+		"""
+		accounts = list(db.sb_details.find())
+		description = ""
+		for index, data in enumerate(accounts):
+			sb = await self.account_details(data["username"].lower(), True)
+			description += "{}{} - {} - {} SB\n".format(0 if index+1 < 10 else "", index+1, data["username"], sb)
+		if not accounts:
+			return await ctx.send("No accounts found.")
+		await ctx.send("```\n{}\n```".format(description))
+    
+		
+	@commands.command()
+	@commands.is_owner()
+	async def nextshow(self, ctx: commands.Context):
+		"""
+		Get Swagbucks Live next show details.
+		"""
+		username = list(db.sb_details.find())[0]["username"]
+		ws = SwagbucksLive(self.client, username)
+		await ws.show_details()
+		
+	# @commands.command()
+	# async def sbtoken(self, ctx, email_id: str = None, password: str = None):
+	# 	if ctx.guild:
+	# 		return await ctx.send("Please use this command in Private Messages.")
+	# 	if not email_id or not password:
+	# 		return await ctx.send("Username or Password is required to login to Swagbucks.")
+	# 	token = await self.login(email_id, password, "GET")
+	# 	await ctx.send("```\n{}\n```".format(token))
+		
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix = ">", intents = intents, strip_after_prefix = True, case_insensitive = True)
+# client.remove_command('help')
+client.add_cog(SwagbucksTrivia(client))
+# server.keep_alive()
+client.run(config.BOT_TOKEN)
