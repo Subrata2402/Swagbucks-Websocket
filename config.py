985e24de1c7fbdb287e3a496c418b9116d939758
Subrata Das <68939201+Subrata2402@users.noreply.github.com>
2022-10-16 19:52:39 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+BOT_TOKEN = "" # Put here bot token
+WEBHOOK_URL = "" # Put message channel webhook url 
+OWNER_ID = 000000000000000000 #  Replace it with your discord id 
+USER_ID = 000000000000000000 # Replace it with discord id which user give ans in chat
+CHANNEL_ID = 0000000000000000 # Replace it with discord channel id to run bot in this channel
